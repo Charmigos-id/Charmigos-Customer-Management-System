a963c7bc200319960394d3e2ca89939b4f061d29
fairuza-alf <prasetyofairuza@gmail.com>
2026-06-29 11:07:39 +0700

    Update storage.py

--- storage.py
+++ storage.py
@@ -40,7 +40,7 @@ OAUTH_TOKEN_FILE     = "oauth_token.pkl"
 
 GDRIVE_FOLDER        = "1YlPRqG1Gh51-_YzXjrAVA4jbvA1KtD5H"
 GDRIVE_HASIL_FOLDER  = "1uhkQXn7PK4JUXEHBCpJccuSibRhJdRqU"
-GSHEET_ID            = "1A_1954pVqqjwX6-Eux0YHDOibJk0DiFTh__JTwh6DIo"
+GSHEET_ID            = "1mgPJzxCnUnAbNnG32-pCuZ5Kjvym62Rt"
 GSHEET_TAB           = "Transaksi"
 GSHEET_GABUNGAN_TAB  = "Transaksi_Gabungan"
 
